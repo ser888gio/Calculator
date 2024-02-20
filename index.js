6a7f4d0f35e2ba33ed17819f6d67611556b5c01c
nicas <nica.sergiu@seznam.cz>
2024-02-20 19:33:57 +0100

    Added functions, changed the css (need to work on responsivness)

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-//const all_buttons = document.querySelectorAll(".toArray");
 let arrayCalculator = [];
 
-/*all_buttons.forEach(bt =>{
-    bt.addEventListener('click', (e) =>{
-        arrayCalculator.push(e.target.innerHTML);
-        console.log(arrayCalculator);
-    })
-})
-
-
-for (let i = 0; i < all_buttons.length; i++){
-    all_buttons[i].addEventListener("click", function(e){
-        alert("You clicked " + e.target.id)
-    })
-}
- */
 function clicked(e){
     let newBtn = e.target.value;
     arrayCalculator.push(newBtn);
-    console.log(arrayCalculator)
+    console.log(arrayCalculator);
+    updateDisplay();
 }
 
 function calculate(arrayCalculator){
     for (let i = 0; i < arrayCalculator.length; i++){
+        if (arrayCalculator[i].isInteger()){
+
+        }
+        else if (arrayCalculator[i] in '+-*/'){
+            if (arrayCalculator[i] === '+'){
+                let result = arrayCalculator[i-1] + arrayCalculator[i+1];
+                console.log(result)
+            }
+        }
 
     }
 }
@@ -30,13 +25,16 @@ function calculate(arrayCalculator){
 //button Clear Last functionality
 function clearLast(){
     arrayCalculator.splice(-1);
+    updateDisplay();
 }
 
 function clearDisplay(){
     arrayCalculator = [];
+    updateDisplay();
 }
 
 
 //see the displayed result
-function displayResult(){
+function updateDisplay() {
+    document.getElementById('display').value = arrayCalculator.join(' ');
 }
