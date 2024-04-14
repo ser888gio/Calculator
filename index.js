e374bbf426f3e459e21b52eec48d4fff3af154a2
nicas <nica.sergiu@seznam.cz>
2024-04-14 10:10:40 +0100

    Finishing the Shunting algorithm. Adding more buttons to HTML

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 let arrayCalculator = [];
-let numberStringOne = '';
-let numberStringTwo = '';
-let operation = '';
-let currentNumber = '';
-
 
 function clicked(e) {
     let newBtn = e.target.value;
@@ -12,107 +7,121 @@ function clicked(e) {
     updateDisplay();
 }
 
-function calculate() {
+//Defining operators precedence and associative property and creates an object
+const operators = {
+    '^': {
+        prec: 4,
+        assoc: 'right'
+    },
+    '*': {
+        prec: 3,
+        assoc: 'left',
+    },
+    '/': {
+        prec: 3,
+        assoc: 'left',
+    },
+    '+': {
+        prec: 2,
+        assoc: 'left',
+    },
+    '-': {
+        prec: 2,
+        assoc: 'left',
+    },
+};
 
-    //Defining operators precedence and associative property and creates an object
-    const operators = {
-        '*': {
-            prec: 3,
-            assoc: 'left',
-        },
-        '/': {
-            prec: 3,
-            assoc: 'left',
-        },
-        '+': {
-            prec: 2,
-            assoc: 'left',
-        },
-        '-': {
-            prec: 2,
-            assoc: 'left',
-        },
+//checks if something is true
+const assert = (predicate) =>{
+    if(predicate) return;
+    throw new Error('Assertion failed due to invalid token')
+}
+
+const toRPN = (input) => {
+    const opSymbols = Object.keys(operators);
+    const stack = []; //primarily for operators
+    let output = ''; //primarily for numbers
+
+    //returns the last element in stack
+
+    const lastElement = () => {
+        return stack.at(-1);
     };
 
-    const toRPN = (input) => {
-        const opSymbols = Object.keys(operators);
-        const stack = []; //primarily for operators
-        let output = ''; //primarily for numbers
+    //adds token to the output string
+    const addToOutput = (token) => {
+        output += ' ' + token;
+    };
 
-        //returns the last element in stack
-        const peek = () => {
-            return stack.at(-1);
-        };
+    //removes the last element form stack
+    const handlePop = () => {
+        return stack.pop();
+    }
 
-        //adds token to the output string
-        const addToOutput = (token) => {
-            output += ' ' + token;
-        };
+    //Pushes the token to the right category
+    const handleToken = (token) => {
+        switch (true) {
+            // if it is a number a function to add to the output is called
+            case !isNaN(parseFloat(token)):
+                addToOutput(token);
+                break;
+            // testing if token is in operators object as a key.
+            case opSymbols.includes(token):
+                const o1 = token;
+                let o2 = lastElement(); //operator 2 is checked if it is on the top
 
-        const handlePop = () => {
-            return stack.pop();
-        }
-
-        //Pushes the token to the right category
-        const handleToken = (token) => {
-            switch (true) {
-                // if it is a number a function to add to the output is called
-                case !isNaN(parseFloat(token)):
-                    addToOutput(token);
-                    break;
-                // testing if token is in operators object as a key.
-                case opSymbols.includes(token):
-                    const o1 = token;
-                    let o2 = peek(); //operator 2 is checked if it is on the top
-
-                    //checks if the operator 2 can be pushed to the output string
-                    while (
-                        o2 !== undefined &&
-                        o2 !== '(' &&
-                        //example o2 = * and o1 = +. '*' has prec = 3 and '+' has prec = 2
-                        (operators[o2].prec > operators[o1].prec ||
+                //checks if the operator 2 can be pushed to the output string
+                while (
+                    o2 !== undefined &&
+                    o2 !== '(' &&
+                    //example o2 = * and o1 = +. '*' has prec = 3 and '+' has prec = 2
+                    (operators[o2].prec > operators[o1].prec ||
                         //example o1 = + and o2 = -. Both have the same prec, but the '+' will come in front of '-'
-                            (operators[o2].prec === operators[o1].prec &&
-                                operators[o1].assoc === 'left'))
-                        ) {
-                        addToOutput(handlePop());
-                        o2 = peek();
-                    }
-                    stack.push(o1);
-                    break;
-                //'(' pushing into the stack
-                case token === '(':
-                    stack.push(token);
-                    break;
+                        (operators[o2].prec === operators[o1].prec &&
+                            operators[o1].assoc === 'left'))
+                    ) {
+                    addToOutput(handlePop());
+                    o2 = lastElement();
+                }
+                stack.push(o1);
+                break;
+            //'(' pushing into the stack
+            case token === '(':
+                stack.push(token);
+                break;
 
-                case token === ')':
-                    let topOfStack = peek();
-                    while (topOfStack !== '(') {
-                        assert(stack.length !== 0);
-                        addToOutput(handlePop());
-                        topOfStack = peek();
-                    }
-                    assert(peek() === '(');
-                    handlePop();
-                    break;
-                default:
-                    throw new Error(`Invalid token: ${token}`);
-            }
-        };
+            case token === ')':
+                let topOfStack = lastElement();
+                while (topOfStack !== '(') {
+                    //checks if stack is not empty
+                    assert(stack.length !== 0);
 
-        for (let i of input) {
-            if (i === ' ') continue;
-
-            handleToken(i);
+                    addToOutput(handlePop());
+                    topOfStack = lastElement();
+                }
+                assert(lastElement() === '(');
+                handlePop();
+                break;
+            default:
+                throw new Error(`Invalid token: ${token}`);
         }
-
-        while (stack.length !== 0) {
-            assert(peek() !== '(');
-            addToOutput(stack.pop());
-        }
-
-        return output;
     };
+
+    for (let i of input) {
+        if (i === ' ') continue;
+        handleToken(i);
+    }
+
+    while (stack.length !== 0) {
+        assert(lastElement() !== '(');
+        addToOutput(stack.pop());
+    }
+    debugger
+    return output;
+};
+
+function calculate() {
+    let result = toRPN(arrayCalculator)
 }
 
 //clears the last digit from display, by removing the last element from arrayCalculator
@@ -131,6 +140,7 @@ function clearDisplay() {
 function updateDisplay(result) {
     if (result !== undefined) {
         document.getElementById('display').value = result;
+        console.log(calculate())
     } else {
         document.getElementById('display').value = arrayCalculator.join(' ');
     }
